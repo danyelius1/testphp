5be9e3751e6083833a0f0c78fe21e27851edeaf7
VARTOTOJAS <@>
2024-01-25 16:22:37 +0200

    jhj

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_POST["ok"])){
+    echo $_POST["vardas"];
+    echo $_POST["pavarde"];
+    echo $_POST["elpastas"];
+    echo $_POST["password"];
+}
+
--- index.php
+++ index.php
@@ -9,10 +9,26 @@
 </head>
 <body>
 
+<form action="connection.php" method="post">
+
+    <label for="vardas">jūsų vardas:</label>
+    <input type="text" name="vardas" id="vardas" required>
+
+    <label for="pavarde">jūsų pavardė:</label>
+    <input type="text" name="pavarde" id="pavarde" required>
+
+    <label for="elpastas">el.paštas:</label>
+    <input type="email" name="elpastas" id="elpastas" required>
+
+    <label for="password">slaptažodis:</label>
+    <input type="password" name="password" id="password" required>
+
+    <input type="submit" id="ok" name="ok">
+
+</form>
+
 </body>
 </html>
 
 
-<?php
-    echo "sdhs";
-    ?>
+
