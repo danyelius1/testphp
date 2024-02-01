6edd83f210558661d1a5c8c64347deb8e0a8c487
VARTOTOJAS <@>
2024-02-01 17:02:11 +0200

    @

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 
-include "connection.php";
+include "includes/connection.php";
 
 if (isset($_POST["ok"])){
 
@@ -29,7 +29,7 @@ if (isset($_POST["ok"])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title></title>
     <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
